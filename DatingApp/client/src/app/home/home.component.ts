9d914156f14310fb75b444bf784fb761537e6ae0
darre <darrenswan4@gmail.com>
2022-04-09 14:32:06 +0100

    played with sending data to and from parent to child

--- DatingApp/client/src/app/home/home.component.ts
+++ DatingApp/client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,13 +9,23 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   registerMode = false;
 
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
   registerToggle(){
-    
+    this.registerMode = !this.registerMode;
+  }
+
+  // getUsers() { //get back an array of users as response from api and were setting the users var in this class to the array of users
+  //   this.http.get('https://localhost:5001/api/users').subscribe(users => this.users = users); //this.users is class var. calling response array "users"
+  // }
+
+  cancelRegisterMode(event:boolean){
+    this.registerMode = event;
   }
 
 }
